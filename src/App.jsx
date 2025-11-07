e7aa591322b8e1714b697d756af85420b77bf46a
LeeSeungHoon <ysh73900@naver.com>
2025-11-07 15:41:56 +0900

    ✨ Feat: 앱 새로고침 시 로그인 상태 유지 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./layout/Layout";
 import MainPage from "./pages/MainPage/MainPage";
+import BlogPage from "./pages/BlogPage/BlogPage";
+import PostDetailPage from "./pages/PostDetailPage/PostDetailPage";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { loadAuthStateFromLocalStorage } from "./features/authSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // 컴포넌트가 처음 마운트될 때 (앱 시작 또는 새로고침 시)
+    // localStorage에 저장된 인증 상태를 Redux 스토어로 불러온다.
+    dispatch(loadAuthStateFromLocalStorage());
+  }, [dispatch]);
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           {/* 2. 로그인이 필요 없는 공개 페이지 */}
           <Route index element={<MainPage />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/blog/:slug" element={<PostDetailPage />} />
 
           {/* 3. 로그인이 반드시 필요한 비공개 페이지 */}
           {/* <Route element={<PrivateRoute />}>
