05e64e2cee7e91b5dc21483004516ffe6ce4c063
LeeSeungHoon <kim73900@naver.com>
2025-11-14 16:59:20 +0900

    ♻️ Refactor: 눈 내리는 효과를 MainPage에서 Layout으로 이동

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,9 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+import FallingSnow from "../components/FallingSnow";
 
 const Layout = () => {
   return (
     <div>
+      <div id="snow" className="fixed inset-0">
+        <FallingSnow />
+      </div>
+
       <Header />
       <Outlet />
     </div>
